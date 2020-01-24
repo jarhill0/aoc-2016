20edfbda309623fd1274a0b5444ba094b7261880
jarhill0 <git@reeshill.net>
2020-01-23 21:53:59 -0800

    d1p2

--- src/solutions/day01.rs
+++ src/solutions/day01.rs
@@ -1,3 +1,5 @@
+use std::collections::HashSet;
+
 use crate::solutions::Solution;
 use crate::util::int;
 
@@ -31,9 +33,63 @@ impl Solution for Day1 {
         println!("{}", x_offset.abs() + y_offset.abs());
     }
 
-    fn part2(&self, input: String) {}
+    fn part2(&self, input: String) {
+        let mut walker = Walker::new();
+        let mut visited: HashSet<(i32, i32)> = HashSet::new();
+
+        'outer: for movement in input.split(", ") {
+            let (turn, magnitude) = movement.split_at(1);
+            let magnitude = int(magnitude).unwrap();
+            walker.turn(turn);
+            for _ in 0..magnitude {
+                if !visited.insert((walker.x, walker.y)) {
+                    // already present
+                    break 'outer;
+                }
+
+                walker.walk(1);
+            }
+        }
+        println!("{}", walker.x.abs() + walker.y.abs());
+    }
 }
 
+struct Walker {
+    x: i32,
+    y: i32,
+    direction: Direction,
+}
+
+impl Walker {
+    fn new() -> Walker {
+        Walker {
+            x: 0,
+            y: 0,
+            direction: Direction::North,
+        }
+    }
+
+    fn turn(&mut self, turn: &str) {
+        match turn {
+            "R" => self.direction = self.direction.right(),
+            "L" => self.direction = self.direction.left(),
+            _ => panic!("Bad direction."),
+        }
+    }
+
+    fn walk(&mut self, magnitude: i32) {
+        use Direction::*;
+
+        match self.direction {
+            East => self.x += magnitude,
+            West => self.x -= magnitude,
+            North => self.y += magnitude,
+            South => self.y -= magnitude,
+        }
+    }
+}
+
+#[derive(Copy, Clone)]
 enum Direction {
     North,
     East,
