6df9e509aab5709e329bcb566390186643512bf2
jarhill0 <git@reeshill.net>
2020-01-25 12:33:41 -0800

    d3p2

--- src/solutions/day03.rs
+++ src/solutions/day03.rs
@@ -13,7 +13,14 @@ impl Solution for Day3 {
         );
     }
 
-    fn part2(&self, input: String) {}
+    fn part2(&self, input: String) {
+        print!(
+            "{}",
+            parse_input_2(&input)
+                .filter(satisfies_triangle_inequality)
+                .count()
+        );
+    }
 }
 
 fn satisfies_triangle_inequality(triangle: &(i32, i32, i32)) -> bool {
@@ -39,4 +46,62 @@ fn parse_input(input: &str) -> impl Iterator<Item = (i32, i32, i32)> + '_ {
             int(words.next().unwrap()).unwrap(),
         )
     })
+}
+
+fn parse_input_2(input: &str) -> impl Iterator<Item = (i32, i32, i32)> + '_ {
+    ColumnIterator {
+        line_iterator: input.split('\n'),
+        triplet_a: None,
+        triplet_b: None,
+    }
+}
+
+struct ColumnIterator<'a, T: Iterator<Item = &'a str>> {
+    line_iterator: T,
+
+    triplet_a: Option<(i32, i32, i32)>,
+    triplet_b: Option<(i32, i32, i32)>,
+}
+
+impl<'a, T: Iterator<Item = &'a str>> Iterator for ColumnIterator<'a, T> {
+    type Item = (i32, i32, i32);
+
+    fn next(&mut self) -> Option<Self::Item> {
+        // stored an answer from a previous iteration…
+        if let Some(triplet) = self.triplet_a {
+            self.triplet_a = None;
+            return Some(triplet);
+        }
+        if let Some(triplet) = self.triplet_b {
+            self.triplet_b = None;
+            return Some(triplet);
+        }
+
+        let mut lines = vec![];
+
+        for _ in 0..3 {
+            let new_line = self.line_iterator.next();
+            lines.push(new_line?.split_whitespace());
+        }
+
+        let mut l1 = lines.pop().unwrap();
+        let mut l2 = lines.pop().unwrap();
+        let mut l3 = lines.pop().unwrap();
+
+        self.triplet_a = Some((
+            int(l1.next().unwrap()).unwrap(),
+            int(l2.next().unwrap()).unwrap(),
+            int(l3.next().unwrap()).unwrap(),
+        ));
+        self.triplet_b = Some((
+            int(l1.next().unwrap()).unwrap(),
+            int(l2.next().unwrap()).unwrap(),
+            int(l3.next().unwrap()).unwrap(),
+        ));
+        Some((
+            int(l1.next().unwrap()).unwrap(),
+            int(l2.next().unwrap()).unwrap(),
+            int(l3.next().unwrap()).unwrap(),
+        ))
+    }
 }
