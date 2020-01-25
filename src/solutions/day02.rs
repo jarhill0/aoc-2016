452a0fc1695656fafe2caca1725d0713ec006b8d
jarhill0 <git@reeshill.net>
2020-01-25 11:33:17 -0800

    d2p2

--- src/solutions/day02.rs
+++ src/solutions/day02.rs
@@ -9,18 +9,28 @@ impl Solution for Day2 {
 
         for sequence in parse_input(&input) {
             key_pad.steps(sequence);
-            code += &key_pad.digit.to_string();
+            code.push(key_pad.as_char());
         }
 
         println!("{}", code);
     }
 
-    fn part2(&self, input: String) {}
+    fn part2(&self, input: String) {
+        let mut code = String::new();
+        let mut key_pad = KeyPad::new();
+
+        for sequence in parse_input(&input) {
+            key_pad.steps(sequence);
+            code.push(key_pad.as_char());
+        }
+
+        println!("{}", code.to_ascii_uppercase());
+    }
 }
 
 #[derive(Copy, Clone)]
 struct KeyPad {
-    digit: u8,
+    digit: u32,
 }
 
 impl KeyPad {
@@ -45,27 +55,43 @@ impl KeyPad {
         }
     }
 
+    fn as_char(&self) -> char {
+        std::char::from_digit(self.digit, 16).unwrap()
+    }
+
     fn up(&mut self) {
-        if self.digit > 3 {
-            self.digit -= 3;
+        self.digit = match self.digit {
+            5 | 2 | 1 | 4 | 9 => self.digit,
+            3 => 1,
+            6 | 7 | 8 | 0xa | 0xb | 0xc => self.digit - 4,
+            0xd => 0xb,
+            _ => panic!("unreachable"),
         }
     }
 
     fn down(&mut self) {
-        if self.digit < 7 {
-            self.digit += 3;
+        self.digit = match self.digit {
+            5 | 0xa | 0xd | 0xc | 9 => self.digit,
+            0xB => 0xD,
+            2 | 3 | 4 | 6 | 7 | 8 => self.digit + 4,
+            1 => 3,
+            _ => panic!("unreachable"),
         }
     }
 
     fn right(&mut self) {
-        if self.digit % 3 != 0 {
-            self.digit += 1;
+        self.digit = match self.digit {
+            1 | 4 | 9 | 0xC | 0xD => self.digit,
+            8 | 7 | 6 | 5 | 3 | 2 | 0xA | 0xB => self.digit + 1,
+            _ => panic!("unreachable"),
         }
     }
 
     fn left(&mut self) {
-        if self.digit % 3 != 1 {
-            self.digit -= 1;
+        self.digit = match self.digit {
+            1 | 2 | 5 | 0xA | 0xD => self.digit,
+            3 | 4 | 6 | 7 | 8 | 9 | 0xB | 0xC => self.digit - 1,
+            _ => panic!("unreachable"),
         }
     }
 }
