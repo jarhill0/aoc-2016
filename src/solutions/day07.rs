925f26ab56f09cb95b63751a57aa1ecbe728e345
jarhill0 <git@reeshill.net>
2020-01-27 17:51:48 -0800

    d7p2

--- src/solutions/day07.rs
+++ src/solutions/day07.rs
@@ -1,3 +1,5 @@
+use std::collections::HashSet;
+
 use crate::solutions::Solution;
 
 pub struct Day7 {}
@@ -8,7 +10,10 @@ impl Solution for Day7 {
         println!("{}", answer);
     }
 
-    fn part2(&self, _input: String) {}
+    fn part2(&self, input: String) {
+        let answer = input.split('\n').filter(supports_ssl).count();
+        println!("{}", answer);
+    }
 }
 
 fn outsides(input: &str) -> impl Iterator<Item = &str> {
@@ -30,4 +35,40 @@ fn has_abba(seq: &str) -> bool {
 
 fn supports_tls(address: &&str) -> bool {
     outsides(address).any(has_abba) && !insides(address).any(has_abba)
+}
+
+fn is_aba(slice: &&str) -> bool {
+    if slice.len() == 3 {
+        let mut slice = slice.chars();
+        let a = slice.next().unwrap();
+        let b = slice.next().unwrap();
+        let c = slice.next().unwrap();
+        a == c && a != b
+    } else {
+        false
+    }
+}
+
+fn section_abas(section: &str) -> HashSet<&str> {
+    (0..section.len() - 2)
+        .map(|i| &section[i..i + 3])
+        .filter(is_aba)
+        .collect()
+}
+
+fn all_abas<'a>(site: impl Iterator<Item = &'a str>) -> HashSet<&'a str> {
+    site.map(section_abas).flatten().collect()
+}
+
+fn supports_ssl(address: &&str) -> bool {
+    let abas = all_abas(outsides(address));
+    let babs = all_abas(insides(address));
+    abas.iter()
+        .any(|aba| babs.iter().any(|bab| aba_match(aba, bab)))
+}
+
+/// Assumes that aba and bab are both ABAs with len 3.
+fn aba_match(aba: &str, bab: &str) -> bool {
+    aba.chars().next().unwrap() == bab.chars().nth(1).unwrap()
+        && aba.chars().nth(1).unwrap() == bab.chars().next().unwrap()
 }
